9faee8c83b10ce21504278aa040d19e1e070d709
geethuharilal <g.thottungalharilal@nhs.net>
2025-01-28 16:09:38 +0000

    last commit

---
+++ subQueryLesson.sql
@@ -0,0 +1,49 @@
+
+/*
+This is a scalar subquery returning a single value to use in the WHERE <column> =
+List the patient stays with the lowest tariff
+*/
+
+SELECT
+	ps.PatientId
+	, ps.Hospital
+	, ps.Tariff
+FROM
+	PatientStay ps
+WHERE
+	ps.Tariff = (
+    SELECT MIN(ps2.Tariff) FROM PatientStay ps2
+   );
+
+   /*
+This is a scalar subquery returning a single value to use in the WHERE <column> =
+List the patient stays with the average tariff
+*/
+
+SELECT
+	ps.PatientId
+	, ps.Hospital
+	, ps.Tariff
+FROM
+	PatientStay ps
+WHERE
+	ps.Tariff = (
+    SELECT avg(ps2.Tariff) FROM PatientStay ps2
+   );
+
+/*
+This is a scalar subquery returning a single value to use in the WHERE <column> =
+List the patient stays greater than the average tariff
+*/
+
+SELECT
+	ps.PatientId
+	, ps.Hospital
+	, ps.Tariff
+FROM
+	PatientStay ps
+WHERE
+	ps.Tariff > (
+    SELECT avg(ps2.Tariff) FROM PatientStay ps2
+   );
+
